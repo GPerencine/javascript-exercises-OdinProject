3c9c75f92877a3f806c7aa59837c0a29261068b2
GPerencine <gabrielperencine1@gmail.com>
2026-07-20 11:36:27 -0300

    Foundations: Solve exercise 05 (Join Strings)

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -3,15 +3,20 @@
  Last name: Stevenson
  This year: 1965
  Birth year: 1947
-
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+
+ Add your code right below, good luck!
 */
+const firstName = "Carlos"
+const lastName = "Stevenson"
+const fullName = firstName + ' ' + lastName
+const thisYear = 1965
+const birthYear = 1947
+const age = thisYear - birthYear
 
-// Add your code right below, good luck!
+//const greeting = `Hello! My name is ${firstName + ' ' + lastName} and I am ${thisYear - birthYear} years old.`
 
-
-
-
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
 // Do not change this
 module.exports = {
