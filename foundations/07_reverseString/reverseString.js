ff3f2efbd0197e59d92f866720680e26f5ae976e
GPerencine <gabrielperencine1@gmail.com>
2026-07-21 22:34:48 -0300

    Foundations: Solve exercise 07 (Reverse String)

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // gabarito: return string.split("").reverse().join("");
+    let splited = str.split("");
+    let reversed = "";
+    for (let i = splited.length - 1; i >= 0; i--){
+        reversed += splited[i];
+    }
+    
+    return reversed;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
