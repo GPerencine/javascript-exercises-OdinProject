24f6e3fcc99124745e850889c03557e2c2cc463c
GPerencine <gabrielperencine1@gmail.com>
2026-07-21 22:16:47 -0300

    Foundations: Solve exercise 06 (Repeat String)

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let repeat = "";
+    if (num < 0){
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++){
+        repeat += word;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
