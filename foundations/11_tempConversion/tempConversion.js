b9c6fe69c7cc1ffdbbf07506189775eefb84a5c5
GPerencine <gabrielperencine1@gmail.com>
2026-07-22 00:08:06 -0300

    Foundations: Solve exercise 11 (Temp Conversion)

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let degreesFahrenheit = (temp - 32) / 1.8;
+  if (Number.isInteger(degreesFahrenheit)){
+    return degreesFahrenheit;
+  }
+  return Number(degreesFahrenheit.toFixed(1));
+  // ou +degreesFahrenheit.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let degreesCelsius = (temp * 1.8) + 32;
+  if (Number.isInteger(degreesCelsius)){
+    return degreesCelsius;
+  }
+  return +degreesCelsius.toFixed(1);  
+  // ou Number(degreesCelsius.toFixed(1))
 };
 
 // Do not edit below this line
