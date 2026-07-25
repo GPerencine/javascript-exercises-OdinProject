9ad4c5ec11ae083955a55d7b2ea77579dfa399d3
GPerencine <gabrielperencine1@gmail.com>
2026-07-24 21:51:37 -0300

    Foundations: Solve exercise 13 (Palindromes)

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const word = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+    const palindrome = word.split('').reverse().join('')
+    return word === palindrome;
 };
 
 // Do not edit below this line
