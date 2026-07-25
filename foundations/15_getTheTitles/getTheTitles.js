9e33027a29e8ce590764f60ed155182b91511902
GPerencine <gabrielperencine1@gmail.com>
2026-07-24 23:36:34 -0300

    Foundations: Solve exercise 15 (Get The Titles)

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let listBooks = []
+    for (const i in books){
+        listBooks.push(books[i].title)
+    }
+    return listBooks;
 
+    /*
+    for (const book of books) {
+        listBooks.push(book.title);
+    }
+    */
+
+    //return books.map((book) => book.title);
 };
 
 // Do not edit below this line
