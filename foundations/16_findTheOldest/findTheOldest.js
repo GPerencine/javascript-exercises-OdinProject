e706070b85f3427c6d51e9a50d831a2719c37753
GPerencine <gabrielperencine1@gmail.com>
2026-07-25 00:24:51 -0300

    Foundations: Solve exercise 16 (Find The Oldest)

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
 
+    persons.forEach(element => {
+        if (element.yearOfDeath === undefined){
+            element.age = new Date().getFullYear() - element.yearOfBirth;
+        }else{
+            element.age = element.yearOfDeath - element.yearOfBirth; 
+        } 
+    });
+
+    const oldest = persons.reduce( (oldest, current) => {
+        return oldest.age > current.age ? oldest : current
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
