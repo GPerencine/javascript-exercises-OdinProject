eca3578ba7b63dd3f26259b651344558264f734e
GPerencine <gabrielperencine1@gmail.com>
2026-07-24 21:32:53 -0300

    Foundations: Solve exercise 12 (Calculator)

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (i in arr){
+    sum += arr[i];
+  }
+  return sum;
+  //return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (i in arr){
+    product *= arr[i];
+  }
+  return product;
+  //return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let factor = 1;
+  for (let i = 1; i <= num1; i++){
+    factor *= i;
+  }
+  return factor;
 };
 
 // Do not edit below this line
