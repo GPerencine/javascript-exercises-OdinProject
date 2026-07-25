063ab6f90b6ed871501ba15a5c8351c8ce6fb834
GPerencine <gabrielperencine1@gmail.com>
2026-07-24 22:46:06 -0300

    Foundations: Solve exercise 14 (Fibonacci)

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if (position < 0) return 'OOPS';
+    
+    let current = 0;
+    let next = 1;
+    
+    for (let i = 0; i < Number(position); i++){
+        let temp = current;   //0 1 1 2
+        current = next;       //1 1 2 3
+        next = current + temp;//1 2 3 5
+        
+        //[current, next] = [next, current + next];
+    }
+    return current;
 };
 
 // Do not edit below this line
